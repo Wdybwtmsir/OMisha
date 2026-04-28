cbc12464c1e89daf0220abc999848a4381894160
Sergey Khromenkov <khromenkov00@bk.ru>
2026-04-28 11:35:28 +0200

    Добавьте файлы проекта.

---
+++ OMisha/Program.cs
@@ -0,0 +1,13 @@
+using ChatHub;
+using OMisha.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapHub<GameHub>("/gameHub");
+
+app.Run();
